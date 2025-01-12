5f22136578eec12f925479591394d1804974a690
jomar godinez <jomargodinez70@gmail.com>
2025-01-12 08:05:12 +0800

    install: adding spatie media package for file upload

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,6 +44,13 @@ return [
             'visibility' => 'public',
             'throw' => false,
         ],
+        'media' => [
+            'driver' => 'local',
+            'root'   => public_path('media'),
+            'url'    => env('APP_URL').'/media',
+            'visibility' => 'public',
+            'throw' => false,
+        ],
 
         's3' => [
             'driver' => 's3',
