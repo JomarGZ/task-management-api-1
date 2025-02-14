47bff359501d9b638784349fd8b3bcfd50d80b1c
jomar godinez <jomargodinez70@gmail.com>
2025-02-14 14:13:38 +0800

    Update ProjectObserver.php

--- app/Observers/v1/ProjectObserver.php
+++ app/Observers/v1/ProjectObserver.php
@@ -10,48 +10,8 @@ class ProjectObserver
 {
     public function creating(Project $project): void
     {
-        // Example: Auto-fill a field before saving
        $project->status = Statuses::NOT_STARTED->value;
        $project->priority = PriorityLevel::LOW->value;
     }
-
-    /**
-     * Handle the Project "created" event.
-     */
-    public function created(Project $project): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Project "updated" event.
-     */
-    public function updated(Project $project): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Project "deleted" event.
-     */
-    public function deleted(Project $project): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Project "restored" event.
-     */
-    public function restored(Project $project): void
-    {
-        //
-    }
-
-    /**
-     * Handle the Project "force deleted" event.
-     */
-    public function forceDeleted(Project $project): void
-    {
-        //
-    }
+  
 }
