1a0c5aec80bd87c98457cf7f8b96e9af8cd4dfa0
jomar godinez <jomargodinez70@gmail.com>
2025-03-07 21:27:39 +0800

    feat: adjust user task retrieval  allowing any  assigned user to get their task

--- app/Http/Controllers/api/v1/Tasks/UserTasksController.php
+++ app/Http/Controllers/api/v1/Tasks/UserTasksController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api\v1\Tasks;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\api\v1\Tasks\TaskResource;
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Http\Request;
 use function Laravel\Prompts\select;
 
@@ -12,10 +13,15 @@ class UserTasksController extends Controller
 {
     public function index(Request $request)
     {
+        $userId = $request->user_id;
+        $assigneeId = null;
+        if ($request->has('user_id') && !empty($userId)) {
+            $assigneeId = User::select('id')->findOrFail($userId)->id;
+        }
         $tasks = Task::query()
             ->select(['id', 'title', 'status', 'priority_level', 'project_id'])
             ->with('project:id,name')
-            ->assignedTo()
+            ->assignedTo($assigneeId)
             ->filterBySearch($request->search)
             ->filterByStatus($request->status)
             ->filterByPriorityLevel($request->priority_level)
